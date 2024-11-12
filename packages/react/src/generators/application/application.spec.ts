2b193c10428584bfd7e7fb5ae16d2cbf634e454d
Jack Hsu <jack.hsu@gmail.com>
2024-11-12 09:26:59 -0500

    feat(react): update extends for tsconfig files

--- packages/react/src/generators/application/application.spec.ts
+++ packages/react/src/generators/application/application.spec.ts
@@ -1443,7 +1443,7 @@ describe('app', () => {
               "@nx/react/typings/image.d.ts",
             ],
           },
-          "extends": "./tsconfig.json",
+          "extends": "../tsconfig.base.json",
           "include": [
             "vite.config.ts",
             "vite.config.mts",
